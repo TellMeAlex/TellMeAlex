06d4c7e227d01e1c0714e92220fdd012574e3957
Alejandro <alexdragoak@gmail.com>
2020-12-15 16:06:55 +0100

    spotify conect

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const readme = `\
 
 <p align="center">
  <img  src="https://camo.githubusercontent.com/992babdffd8c74a1502de375fbdf7e4d54773242/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f53576f536b4e36447854737a71494b4571762f67697068792e676966" align="center" alt="working" />
-</p>
+ [[https://spotify-github-profile.vercel.app/api/view.svg?uid=alexdrago&redirect=true][https://spotify-github-profile.vercel.app/api/view.svg?uid=alexdrago&cover_image=true&theme=natemoo-re)]]
+ </p>
 <h2 align="center">Programming Languages 🌐</h2>
 
 
