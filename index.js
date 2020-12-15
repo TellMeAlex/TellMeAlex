7227f40c7bcc522e4d1810a801ea8131d411f993
Alejandro <alexdragoak@gmail.com>
2020-12-15 01:53:13 +0100

    fix contact

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const readme = `\
  <img  src="https://camo.githubusercontent.com/992babdffd8c74a1502de375fbdf7e4d54773242/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f53576f536b4e36447854737a71494b4571762f67697068792e676966" align="center" alt="working" />
 </p>
 <h2 align="center">Programming Languages 🌐</h2>
-
 ---
 <p align="center">
 <img src="https://devicons.github.io/devicon/devicon.git/icons/html5/html5-original.svg" alt="Html" width="55" height="55"/> 
@@ -53,9 +52,8 @@ const readme = `\
 <h3> 🤝🏻 Connect with Me </h3>
 
 <p align="center">
-<img src="https://devicons.github.io/devicon/devicon.git/icons/html5/html5-original.svg" alt="Html" width="55" height="55"/> 	<a href="https://www.mehagomayor.com" target="_blank"><img alt="Website" src="https://img.shields.io/badge/Website-www.MeHagoMayor.com-blue?style=flat&logo=google-chrome"></a>
-<img src="https://devicons.github.io/devicon/devicon.git/icons/css3/css3-original.svg" alt="CSS" width="55" height="55"/> 	<a href="https://www.linkedin.com/in/alejandro-de-la-fuente/" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/LinkedIn-@AlejadnroDeLaFuente-blue?style=flat&logo=linkedin"></a>
-<img src="https://devicons.github.io/devicon/devicon.git/icons/javascript/javascript-original.svg" alt="javascript" width="55" height="55"/> 	<a href="mailto:llamamealex@gmail.com"><img alt="Email" src="https://img.shields.io/badge/Email-llamamealex@gmail.com-blue?style=flat&logo=gmail"></a>
-<img src="https://devicons.github.io/devicon/devicon.git/icons/nodejs/nodejs-original.svg" alt="nodejs" width="55" height="55"/> 	
+<a href="https://www.mehagomayor.com" target="_blank"><img alt="Website" src="https://img.shields.io/badge/Website-www.MeHagoMayor.com-blue?style=flat&logo=google-chrome"></a>
+<a href="https://www.linkedin.com/in/alejandro-de-la-fuente/" target="_blank"><img alt="LinkedIn" src="https://img.shields.io/badge/LinkedIn-@AlejadnroDeLaFuente-blue?style=flat&logo=linkedin"></a>
+<a href="mailto:llamamealex@gmail.com"><img alt="Email" src="https://img.shields.io/badge/Email-llamamealex@gmail.com-blue?style=flat&logo=gmail"></a>
 `
 console.log(readme)
